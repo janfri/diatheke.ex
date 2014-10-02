9af5b897cb2a014c9d05d0ad3ae29042a6095185
Jan Friedrich <janfri26@gmail.com>
2014-10-02 16:36:17 +0200

    Add documentation.

--- lib/diatheke.ex
+++ lib/diatheke.ex
@@ -1,9 +1,24 @@
 defmodule Diatheke do
 
+  @moduledoc """
+  This library is a wrapper of the diatheke command-line client of the sword project.
+  """
+
+  @doc "Returns a list of installed sword modules."
   def mods do
     call("system", "modulelistnames") |> String.split(~r/\n/) |> Enum.reject(&(&1 == ""))
   end
 
+  @doc """
+  Returns a list of maps for the verses of the given key.
+  ## Examples
+      iex> Diatheke.passage("KJV", "Jh 1:1-3")
+      [%{key: "John 1:1",
+         text: "In the beginning was the Word, and the Word was with God, and the Word was God."},
+       %{key: "John 1:2", text: "The same was in the beginning with God."},
+       %{key: "John 1:3",
+         text: "All things were made by him; and without him was not any thing made that was made."}]
+  """
   def passage(mod, key) do
     call(mod, key) |> parse_passage
   end
