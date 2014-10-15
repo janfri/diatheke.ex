d452e4c8d5b8dd42a059f9fcfc2e5937fb09c09b
Jan Friedrich <janfri26@gmail.com>
2014-10-15 20:56:25 +0200

    Add tests for edge cases.

--- test/diatheke_test.exs
+++ test/diatheke_test.exs
@@ -28,4 +28,16 @@ defmodule DiathekeTest do
     assert ["John 1:42"] = res
   end
 
+  test "correct split with colon in text" do
+    [res | _]  = Diatheke.passage("KJV", "Joh 1:39")
+    assert "John 1:39" = res[:key]
+    text = "He saith unto them, Come and see. They came and saw where he dwelt, and abode with him that day: for it was about the tenth hour."
+    assert text == res[:text]
+  end
+
+  test "correct split on line break in text" do
+    res = Diatheke.passage("KJV", "Joh 1:15")
+    assert ["John 1:15"] = Enum.map(res, &(&1[:key]))
+  end
+
 end
